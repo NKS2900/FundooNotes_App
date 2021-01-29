f7b7b037f4598b354ded1a0a140eb457dc138ec0
NIJMA SAYYAD <nksayyad2900@outlook.com>
2021-01-29 08:24:28 +0530

    [NIJAM] Add: MSMQ added for masseage sending.

---
+++ MSMQ_Email/Reciver.cs
@@ -0,0 +1,26 @@
+﻿// -----------------------------------------------------------------------------------------------------
+// <copyright file="Reciver.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nijam Sayyad"/>
+// -----------------------------------------------------------------------------------------------------
+
+namespace MSMQ_Email
+{
+    using Experimental.System.Messaging;
+    public class Reciver
+    {
+        /// <summary>
+        /// Reciving Massage from MSMQ
+        /// </summary>
+        /// <returns></returns>
+        public string MailReciver()
+        {
+            var reciever = new MessageQueue(@".\Private$\MyQueue");
+            var recieving = reciever.Receive();
+            recieving.Formatter = new BinaryMessageFormatter();
+            string linkToSend = recieving.Body.ToString();
+            return linkToSend;
+        }
+    }
+}
