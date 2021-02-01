66a0331fc53ea8bcaa64ba2f67d485a2456ef7e7
NIJMA SAYYAD <nksayyad2900@outlook.com>
2021-02-02 01:28:58 +0530

    [NIJAM] Add: RetriveAll Labels API added in Controller.

---
+++ FundooNotes/Controllers/LabelController.cs
@@ -0,0 +1,61 @@
+﻿// -----------------------------------------------------------------------------------------------------
+// <copyright file="LabelConroller.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nijam Sayyad"/>
+// -----------------------------------------------------------------------------------------------------
+
+namespace FundooNotes.Controllers
+{
+    using FundooManager;
+    using FundooModel.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+    using System.Collections.Generic;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LabelController : Controller
+    {
+        private readonly ILabelManager manager;
+        public LabelController(ILabelManager manager)
+        {
+            this.manager = manager;
+        }
+
+        [HttpPost]
+        public IActionResult AddLabels([FromBody] LabelModel model)
+        {
+            try
+            {
+                bool result = manager.AddLabel(model);
+                if (result)
+                {
+                    return this.Ok(new ResponseModel<LabelModel>() { Status = true, Masseage = "Retrieve Notes Successfully", Data = model });
+                }
+                else
+                {
+                    return BadRequest(new { success = false, Message = "Somthing went wrong..." });
+                }
+            }
+            catch (Exception ex)
+            {
+                return this.NotFound(new { Status = false, Message = ex.Message });
+            }
+        }
+
+        [HttpGet]
+        public IActionResult RetriveAllLabels()
+        {
+            try
+            {
+                IEnumerable<LabelModel> labels = manager.RetriveLabeles();
+                return this.Ok(labels);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { success = false, Message = ex.Message });
+            }
+        }
+    }
+}
