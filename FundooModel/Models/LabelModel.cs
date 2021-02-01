a4e8c8eb38b5a9c071068764fb33d14121888b1b
NIJMA SAYYAD <nksayyad2900@outlook.com>
2021-02-02 00:28:49 +0530

    [NIJAM] Add: Label Model added.

---
+++ FundooModel/Models/LabelModel.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace FundooModel.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using FundooModel.Models;
+    public class LabelModel
+    {
+        [Key]
+        public int LabelId { get; set; }
+
+        public string LabelName { get; set; }
+
+        [ForeignKey("NoteModel")]
+        public int NoteId { get; set; }
+
+        public NoteModel NoteModel;
+    }
+}
